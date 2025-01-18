213ee54e0104a1c37663d3f77c03c17519076043
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-01-18 22:34:58 +0900

    init: init Project

---
+++ src/app.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ */
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getPost(): Post {
+    return {
+      author: 'kamja',
+      title: 'kamja',
+      content: '감자는 맛있다',
+      likeCount: 10000,
+      commentCount: 999,
+    };
+  }
+}
