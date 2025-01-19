5a4418c6b630dd8ea7cccd17cb8ac2679e37f94d
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-01-19 15:19:07 +0900

    Post 요청 만들기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 /**
  * author: string;
@@ -68,7 +75,25 @@ export class PostsController {
   }
   // 3) POST /posts
   // POST를 생성한다.
+  @Post()
+  postPosts(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
 
+    posts = [...posts, post];
+
+    return post;
+  }
   // 4) PUT /posts/id
   // POST를 변경한다.
 
