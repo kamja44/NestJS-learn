65b19c368b178ff8a1e353ca4789889860dd1350
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-01-19 15:25:30 +0900

    Patch Post 엔드포인트 생성하기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -5,6 +5,7 @@ import {
   NotFoundException,
   Param,
   Post,
+  Put,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 /**
@@ -96,6 +97,32 @@ export class PostsController {
   }
   // 4) PUT /posts/id
   // POST를 변경한다.
+  @Put(':id')
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    if (author) {
+      post.author = author;
+    }
+    if (title) {
+      post.title = title;
+    }
+    if (content) {
+      post.content = content;
+    }
+
+    posts = posts.map((prevPost) => (prevPost.id === +id ? post : prevPost));
+
+    return post;
+  }
 
   // 5) DELETE /posts/:id
   // id에 해당하는 POST를 삭제한다.
