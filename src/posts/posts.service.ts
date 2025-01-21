a27ce02a91ae5a249aabbea34746d645f3f3e9e9
kamja44 <gudgh5007@gmail.com>
2025-01-21 17:09:49 +0900

    FindOne 함수를 이용하여 하나의 데이터만 찾기

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -56,11 +56,15 @@ export class PostsService {
     return this.postsRepository.find();
   }
 
-  getPostById(id: number) {
-    const post = posts.find((post) => post.id === +id);
+  async getPostById(id: number) {
+    const post = await this.postsRepository.findOne({
+      where: {
+        id,
+      },
+    });
 
     if (!post) {
-      throw new NotFoundException(); // nest에서 제공해주는 404에러
+      throw new NotFoundException();
     }
 
     return post;
